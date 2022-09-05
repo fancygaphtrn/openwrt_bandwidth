bdb2018d445033aa2bd347b60f14414b076f9281
fancygaphtrn <fancygaphtrn@gmail.com>
2022-09-05 08:14:08 -0400

    Update traffic_monitor.sh

--- traffic_monitor.sh
+++ traffic_monitor.sh
@@ -2,8 +2,10 @@
 # Bandwidth Download/Upload Rate Counter
 LAN_TYPE='br-lan'
 SLEEP_TIME=5
+# Change host to match your Home assistant IP/port
+HOST='192.168.5.200:8123'
 # Long lived access token from Home assistant
-TOKEN='Insert log lived token from home assistant here'
+TOKEN='Insert long lived token from Home assistant here'
 
 if [ -f /tmp/traffic_monitor.lock ];
 then
@@ -30,17 +32,17 @@ iptables -w -X RRDIPT
 iptables -w -N RRDIPT 2> /dev/null                                          
                                                                                
 #Add the RRDIPT CHAIN to the FORWARD chain (if non existing).               
-echo "Add RRDIPT CHAIN to the FORWARD chain 1"
-iptables -w -L FORWARD --line-numbers -n | grep "RRDIPT" | grep "1" > /dev/null
+echo "Checking RRDIPT CHAIN in the FORWARD chain as 1"
+iptables -w -L FORWARD --line-numbers -n | grep "RRDIPT" | grep "^1" > /dev/null
 if [ $? -ne 0 ]; then                                                       
-  echo "Add RRDIPT CHAIN to the FORWARD chain 2"
+  echo "Checking RRDIPT CHAIN in the FORWARD chain"
   iptables -w -L FORWARD -n | grep "RRDIPT" > /dev/null                  
   if [ $? -eq 0 ]; then                                               
-    echo "Add RRDIPT CHAIN to the FORWARD chain 3"
+    echo "Delete RRDIPT CHAIN from the FORWARD chain"
     iptables -w -D FORWARD -j RRDIPT                               
   fi                                                                  
-echo "Add RRDIPT CHAIN to the FORWARD chain 4"
-iptables -w -I FORWARD -j RRDIPT                                       
+  echo "Add RRDIPT CHAIN to the FORWARD chain as 3"
+  iptables -w -I FORWARD 1 -j RRDIPT                                     
 fi                                                                          
 
 while :
@@ -59,13 +61,13 @@ do
       iptables -w -I RRDIPT -s ${IP} -j RETURN                       
     fi                                                                  
   done                                                                        
-
-  iptables -w -L RRDIPT -vxZ -t filter | fgrep RETURN | sed 's/.local//' | awk -v st="$SLEEP_TIME" '{if (NR % 2 == 1) printf "%s %d ",$8,$2 * (8 / st); else printf "%d\n",$2 * (8 / st);}' | sort | awk 'BEGIN {TU=0; TD=0; printf "{\"attributes\":{\n" } { printf "\"%s\": {\n\"host\": \"%s\",\n\"upload\": \"%d\",\n\"download\": \"%d\"\n},\n",NR,$1,$2,$3; TU=TU+$2; TD=TD+$3;} END { printf "\"9999\":{\n\"host\": \"%s\",\n\"upload\": \"%d\",\n\"download\": \"%d\"}},\n\"state\":\"%s\"}\n","Total", TU, TD, NR}' | sed -r ':L;s=\b([0-9]+)([0-9]{3})\b=\1,\2=g;t L' > /tmp/traffic_monitor.json
+  
+  iptables -w -L RRDIPT -vxZ -t filter | fgrep RETURN | sed 's/.lan//' | awk -v st="$SLEEP_TIME"+1 '{if (NR % 2 == 1) printf "%s %d ",$8,$2 * (8 / st); else printf "%d\n",$2 * (8 / st);}' | sort | awk 'BEGIN {TU=0; TD=0; printf "{\"attributes\":{\n" } { printf "\"%s\": {\n\"host\": \"%s\",\n\"upload\": \"%d\",\n\"download\": \"%d\"\n},\n",NR,$1,$2,$3; TU=TU+$2; TD=TD+$3;} END { printf "\"9999\":{\n\"host\": \"%s\",\n\"upload\": \"%d\",\n\"download\": \"%d\"}},\n\"state\":\"%s\"}\n","Total", TU, TD, NR}' | sed -r ':L;s=\b([0-9]+)([0-9]{3})\b=\1,\2=g;t L' > /tmp/traffic_monitor.json
   if [ $? -ne 0 ]; then   
       echo "JSON output failed"
   fi  
 
-  curl -m 10 -silent -X POST -H "Authorization: Bearer ${TOKEN}" -H "Content-Type: application/json" -d @/tmp/traffic_monitor.json http://192.168.5.200:8123/api/states/sensor.bw_usage > /dev/null	   
+  curl -m 10 -silent -X POST -H "Authorization: Bearer ${TOKEN}" -H "Content-Type: application/json" -d @/tmp/traffic_monitor.json http://${HOST}/api/states/sensor.bw_usage > /dev/null	   
 
   sleep ${SLEEP_TIME}
 done
